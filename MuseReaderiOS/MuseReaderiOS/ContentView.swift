c141763e3f7fdf776d2dc0c414fbb693a9e36e2a
Jack Gruber <jackgruber82@gmail.com>
2026-06-18 22:15:51 -0400

    latest

--- MuseReaderiOS/MuseReaderiOS/ContentView.swift
+++ MuseReaderiOS/MuseReaderiOS/ContentView.swift
@@ -6,14 +6,30 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    @Environment(\.requestReview) private var requestReview
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var appModel = MuseReaderAppModel()
+    @State private var reviewRequestTracker = ReviewRequestTracker()
 
     var body: some View {
         LibraryView(model: appModel)
             .onOpenURL { url in
                 appModel.handleOpenURL(url)
+            }
+            .onChange(of: scenePhase) { _, phase in
+                guard phase == .active,
+                      reviewRequestTracker.shouldRequestReviewAfterAppOpen()
+                else {
+                    return
+                }
+
+                Task { @MainActor in
+                    try? await Task.sleep(nanoseconds: 1_000_000_000)
+                    requestReview()
+                }
             }
     }
 }
